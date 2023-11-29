a49ca264f860a0a7e6d3c18ba4317b0b4b3295c4
shenny85 <shenderson144@ivytech.edu>
2023-11-28 22:32:34 -0500

    initial react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomeWebPage from './Pages/HomeWebPage';
+import Flooring from './LinkToAllJobs/Flooring';
+import HomeSceurity from './LinkToAllJobs/HomeSecurity';
+import HouseKeeping from './LinkToAllJobs/HouseKeeping';
+import InteriorDesign from './LinkToAllJobs/InteriorDesign';
+import LandScaping from './LinkToAllJobs/Landscaping';
+import Mounting from './LinkToAllJobs/Mounting';
+import Plumbering from './LinkToAllJobs/Plumbing';
+import PoolMaint from './LinkToAllJobs/PoolMaint';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+      <div className="App">
+         <h1>Awaits Completion</h1>
+         <div id="page-body">
+            <Routes>
+               <Route path="/" element={<HomeWebPage />}/>
+               <Route path="/floor" element={<Flooring />}/>
+               <Route path="/security" element={<HomeSceurity />}/>
+               <Route path="/house" element={<HouseKeeping />}/>
+               <Route path="/interior" element={<InteriorDesign />}/>
+               <Route path="/land" element={<LandScaping />}/>
+               <Route path="/Mounting" element={<Mounting />}/>
+               <Route path="/Plumbering" element={<Plumbering />}/>
+               <Route path="/Pool" element={<PoolMaint />}/>
+
+            </Routes>
+         </div>
+      </div>
+   </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/Pages/HomeWebPage.js
@@ -0,0 +1,7 @@
+const HomePage = () => {
+    return(
+        <h1>This is the Home page!</h1>
+    )
+}
+
+export default HomePage
